7086bb419e76452e0e396b74d0843caabc540307
Zhongming Li <zhongming.li@kcl.ac.uk>
2022-10-12 15:17:52 +0100

    Complete frequencies exercise

--- frequencies.py
+++ frequencies.py
@@ -3,27 +3,13 @@
 
 def frequencies(items):
     frequencies = {}
-    temp = items
-    current_length = len(temp)
-    for i in range (0, len(temp)-1):
-        if (i+1 == current_length):
-            frequencies[temp[i+1]] = 1
-            break
-        count = 1
-        #if the element is an integer, convert it to string
-        if isinstance(temp[i], int):
-            temp[i] = str(temp[i])
-
-        for k in range (i+1, current_length):
-            #if the element is an integer, convert it to string
-            if isinstance(temp[k], int):
-                temp[k] = str(temp[k])
-            if (temp[k] == temp[i]):
-                count += 1
-                del temp[k]
-
-        frequencies[temp[i]] = count
-        current_lenth = len(temp)
-
+    for item in items:
+        # If the item is an integer, convert it to string
+        if isinstance(item, int):
+            item = str(item)
+        if item in frequencies:
+            frequencies[item] += 1
+        else:
+            frequencies[item] = 1
 
     return frequencies
